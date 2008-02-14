7a89b30e4cb739078263b8dd3561177cf41fcdd2
el_marcu <>
2008-02-14 14:37:30 +0000

    Fixing bugs in the function move of labyrinth.ml

--- labyrinth/labyrinth.ml
+++ labyrinth/labyrinth.ml
@@ -2,10 +2,15 @@
 
 type dir = [`N | `S | `E | `W]
 type dir_rel = [`Left | `Front | `Right | `Back]
+type state = [`Explored | `Cross_roads | `Non_explored]
+type wall  = [`True | `False | `Unknown]
+type square = { s_state : state ref;
+                wall_left: wall ref;
+                wall_top: wall ref; }
 
-let taille_lab = 7;
+let taille_lab = 7
 
-module Coord :
+module Coord =
 struct
   type t = int*int
 
@@ -21,9 +26,9 @@ struct
     [(`N, ((i+1), j)); (`S, ((i-1), j)); (`E, (i, (j+1))); (`W, (i, (j-1)))]
 end
 
-let square_default = {state = `Non_explored;
-                      wall_left = `Unknown;
-                      wall_top = `Unknown;}
+let square_default = {s_state = ref `Non_explored;
+                      wall_left = ref `Unknown;
+                      wall_top = ref `Unknown;}
 
 let lab = Array.make_matrix ((taille_lab * 2) + 1) ((taille_lab * 2) + 1)
   square_default
@@ -33,11 +38,73 @@ let current_pos = ref (taille_lab, taille_lab)
 let robot_orient = ref `N
 
 let verif_in_lab (i,j) =
-  if(i > 2*(Array.length lab)  || i < 0
-     || j > 2*(Array.length lab.(0) || j < 0
+  if i > 2*(Array.length lab)  || i < 0
+     || j > 2*(Array.length lab.(0)) || j < 0
   then
-    raise Not_In_The_Labyrinth;
+    failwith "Not_In_The_Labyrinth"
   ();;
+
+let wall_on (i,j) d =
+  verif_in_lab (i,j);
+  if d = `N
+  then !(lab.(i).(j).wall_top)
+  else if d = `S
+  then !(lab.(i-1).(j).wall_top)
+  else if d = `W
+  then !(lab.(i).(j).wall_left)
+  else !(lab.(i).(j+1).wall_left)
+
+let status (i,j) =
+  verif_in_lab (i,j);
+  !(lab.(i).(j).s_state)
+
+let position () =
+  !current_pos
+
+let robot_dir () =
+  !robot_orient
+
+let rel_dir (dir_convert:dir) =
+  let x = ref 0 in
+  if dir_convert = `E
+  then x := 1;
+  if dir_convert = `S
+  then x := 2;
+  if dir_convert = `W
+  then x := 3;
+  if !robot_orient = `E
+  then x := !x + 3;
+  if !robot_orient = `S
+  then x := !x + 2;
+  if !robot_orient = `W
+  then x := !x + 1;
+  x := !x mod 4;
+  match !x with
+  | 0 -> `Front
+  | 1 -> `Right
+  | 2 -> `Back
+  | _ -> `Left
+
+let abs_dir (dir_convert:dir_rel) =
+  let x = ref 0 in
+  if dir_convert = `Right
+  then x := 1;
+  if dir_convert = `Back
+  then x := 2;
+  if dir_convert = `Left
+  then x := 3;
+  if !robot_orient = `E
+  then x := !x + 1;
+  if !robot_orient = `S
+  then x := !x + 2;
+  if !robot_orient = `W
+  then x := !x + 3;
+  x := !x mod 4;
+  match !x with
+  | 0 -> `N
+  | 1 -> `E
+  | 2 -> `S
+  | _ -> `W
 
 let nbh_explored (i,j) =
   verif_in_lab (i,j);
@@ -59,95 +126,41 @@ let nbh_explored (i,j) =
 let nbh_unexplored (i,j) =
   verif_in_lab (i,j);
   let return = ref [] in
-  if(i < 2*taille_lab && (wall_on (i,j) `N) != `True
-      && (status (i+1,j) = `Non_explored)) then
+  if(i < 2*taille_lab && (wall_on (i,j) `N) <> `True
+     && (status (i+1,j) = `Non_explored)) then
     return := (`N, ((i+1), j)) :: !return;
-  if(i > 0 && (wall_on (i,j) `S) != `True
+  if(i > 0 && (wall_on (i,j) `S) <> `True
       && (status (i-1,j) = `Non_explored)) then
     return := (`S, ((i-1), j)) :: !return;
-  if(j < 2*taille_lab && (wall_on (i,j) `E) != `True
+  if(j < 2*taille_lab && (wall_on (i,j) `E) <> `True
       && (status (i,j+1) = `Non_explored)) then
     return := (`E, (i, j+1)) :: !return;
-  if(j > 0 && (wall_on (i,j) `W) != `True
+  if(j > 0 && (wall_on (i,j) `W) <> `True
       && (status (i,j-1) = `Non_explored)) then
     return := (`W, (i, j-1)) :: !return;
   !return;;
 
-let wall_on (i,j) d =
-  verif_in_lab (i,j);
-  if d = `N
-  then lab.(i).(j).wall_top
-  else if d = `S
-  then lab.(i-1).(j).wall_top
-  else if d = `W
-  then lab.(i).(j).wall_left
-  else lab.(i).(j+1).wall_left
-
-let status (i,j) =
-  verif_in_lab (i,j);
-  lab.(i).(j).state
-
-let position () =
-  !current_pos
-
-let robot_dir () =
-  !robot_orient
-
-let rel_dir dir_convert =
-  let x = ref 0 in
-  if dir_convert = `E
-  then x := 1;
-  if dir_convert = `S
-  then x := 2;
-  if dir_convert = `w
-  then x := 3;
-  if !robot_orient = `E
-  then x := !x + 3;
-  if !robot_orient = `S
-  then x := !x + 2;
-  if !robot_orient = `W
-  then x := !x + 1;
-  x := x mod 4;
-  match !x with
-  | 0 -> `Front
-  | 1 -> `Right
-  | 2 -> `Back
-  | _ -> `Left
-
-let abs_dir dir_convert =
-  let x = ref 0 in
-  if dir_convert = `Right
-  then x := 1;
-  if dir_convert = `Back
-  then x := 2;
-  if dir_convert = `Left
-  then x := 3;
-  if !robot_orient = `E
-  then x := !x + 1;
-  if !robot_orient = `S
-  then x := !x + 2;
-  if !robot_orient = `W
-  then x := !x + 3;
-  x := x mod 4;
-  match !x with
-  | 0 -> `N
-  | 1 -> `E
-  | 2 -> `S
-  | _ -> `W
-
-let set_wall d w =
+let set_wall (d:dir_rel) (w:[`True | `False | `Unknown]) =
   let (i,j) = !current_pos in
   let d_abs = abs_dir d in
-  if d_abs = `N then lab.(i).(j).wall_top = w
-  else if d = `S then lab.(i-1).(j).wall_top = w
-  else if d = `W then lab.(i).(j).wall_left = w
-  else lab.(i).(j+1).wall_left = w
+  if d_abs = `N then lab.(i).(j).wall_top := w
+  else if d_abs = `S then lab.(i-1).(j).wall_top := w
+  else if d_abs = `W then lab.(i).(j).wall_left := w
+  else lab.(i).(j+1).wall_left := w
 
 let move d =
   let (i,j) = !current_pos in
+  if (wall_on (i,j) `N = `True || status(i+1,j) <> `Non_explored)
+    && (wall_on (i,j) `S = `True || status (i-1,j) <> `Non_explored)
+    && (wall_on (i,j) `W = `True || status (i,j-1) <> `Non_explored)
+    && (wall_on (i,j) `E = `True || status (i, j+1) <> `Non_explored)
+  then
+    lab.(i).(j).s_state := `Explored
+  else
+    lab.(i).(j).s_state := `Cross_roads;
   let d_abs = abs_dir d in
   robot_orient := d_abs;
   if d_abs = `N then current_pos := (i+1, j)
-  else if d = `S then current_pos := (i-1, j)
-  else if d = `W then current_pos := (i, j-1)
+  else if d_abs = `S then current_pos := (i-1, j)
+  else if d_abs = `W then current_pos := (i, j-1)
   else current_pos := (i, j+1)
--- labyrinth/labyrinth.mli
+++ labyrinth/labyrinth.mli
@@ -18,6 +18,6 @@ val status : Coord.t -> [`Explored | `Cross_roads | `Non_explored]
 val position : unit -> Coord.t
 val robot_dir : unit -> dir
 val rel_dir : dir -> dir_rel
-val abs_dir : [`Left | `Front | `Right | `Back] -> dir
-val set_wall : [`Left | `Front | `Right] -> bool -> unit
+val abs_dir : dir_rel -> dir
+val set_wall : dir_rel -> [`True | `False | `Unknown] -> unit
 val move :  [`Left | `Front | `Right | `Back] -> unit
