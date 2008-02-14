4acbf2d6d9776b2a223d96d76d333b7e890a7605
chris_77 <>
2008-02-14 10:12:27 +0000

    - Sketch of the rubik module (yet to evolve).

--- rubik/rubik.ml
+++ rubik/rubik.ml
@@ -6,15 +6,42 @@
 (* http://www.math.ucf.edu/~reid/Rubik/index.html
 
    e.g. http://www.math.ucf.edu/~reid/Rubik/optimal_solver.html
+
    http://kociemba.org/cube.htm
+   http://bj.middlebury.edu/~plubans/report
+
+   Supposedly easier introduction to Kociemba algo:
+   http://www.geocities.com/jaapsch/puzzles/compcube.htm
 
    http://cadigweb.ew.usna.edu/~wdj/papers/rubik.pdf
-   http://cadigweb.ew.usna.edu/~wdj/rubik_nts.htm *)
+   http://cadigweb.ew.usna.edu/~wdj/rubik_nts.htm
+
+*)
 
 type generators = F | B | L | R | U | D
 
-module Perm =
+(** Symmetric group of the cube.  Some Rubik cube coordinates are
+    "reduced" using this symmetry group. *)
+module Sym =
 struct
+  type t = int
+
+end
+
+
+module Coord =
+struct
+
+  (** The Cubies level is a simple way of describing a cube position
+      (i.e. an element of the Rubik group).  This level is intended to
+      communicate with the "outside world" and for basic manipulation.
+      It does not need to be as efficient as subsequent coordinate
+      systems.  *)
+  module Cubies =
+  struct
+
+
+  end
 
 
 end
--- rubik/rubik.mli
+++ rubik/rubik.mli
@@ -18,20 +18,28 @@
 (** Rubik cube solvers. *)
 
 type generators = F | B | L | R | U | D
-    (** Standard Rubik group genrators. *)
+    (** Standard Rubik group genrrators.  The letters respectively
+        stand for the clockwise rotations of the faces `Front',
+        `Back', `Left', `Right', `Up' and `Down'. *)
 
 
-(** Efficient encoding of Rubik cube permutations *)
-module Perm :
+(** Various coordinate systems with corresponding multipliction
+    tables.  *)
+module Coord :
 sig
-  type t
-    (** A permutation of the Rubik cube (i.e. an element of the Rubik
-        group).  This is immutable. *)
+  (** The {{:http://kociemba.org/math/cubielevel.htm}cubies
+      coordinates}.  *)
+  module Cubies :
+  sig
+    type t
+      (** A permutation of the Rubik cube (i.e. an element of the Rubik
+          group).  This is immutable. *)
 
-  val mul : t -> generators -> t
-    (** Right multiply by a generator. *)
+    val mul : t -> generators -> t
+      (** Right multiply by a generator. *)
 
-  val is_identity : t -> bool
-    (** The premutation is the Rubik cube home state i.e., the
-    identity (neutral element of the group). *)
+    val is_identity : t -> bool
+      (** The premutation is the Rubik cube home state i.e., the
+          identity (neutral element of the group). *)
+  end
 end
