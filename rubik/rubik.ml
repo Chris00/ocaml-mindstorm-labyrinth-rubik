492f8a63cac08cb6cba9d60a21ee7754356ca300
chris_77 <>
2008-02-16 17:10:55 +0000

    - Implementation of the Cubie coordinates.

--- rubik/rubik.ml
+++ rubik/rubik.ml
@@ -18,7 +18,19 @@
 
 *)
 
-type generators = F | B | L | R | U | D
+(* Enrich List *)
+module List = struct
+  include List
+  let iteri f l =
+    let rec iter i = function
+      | [] -> ()
+      | a :: tl -> f i a; iter (succ i) tl in
+    iter 0 l
+end
+
+
+
+type generator = F | B | L | R | U | D
 
 (** Symmetric group of the cube.  Some Rubik cube coordinates are
     "reduced" using this symmetry group. *)
@@ -29,19 +41,141 @@ struct
 end
 
 
-module Coord =
+(** The Cubies level is a simple way of describing a cube position
+    (i.e. an element of the Rubik group).  This level is intended to
+    communicate with the "outside world" and for basic manipulation.
+    It is not as efficient as subsequent coordinate systems.  *)
+module Cubie =
 struct
+  type corner = URF | UFL | ULB | UBR | DFR | DLF | DBL | DRB
+  let int_of_corner = function
+    | URF -> 0 | UFL -> 1 | ULB -> 2 | UBR -> 3
+    | DFR -> 4 | DLF -> 5 | DBL -> 6 | DRB -> 7
+  let corner_int = [| URF; UFL; ULB; UBR; DFR; DLF; DBL; DRB |]
+  let corner_name = [| "URF"; "UFL"; "ULB"; "UBR"; "DFR"; "DLF"; "DBL"; "DRB" |]
 
-  (** The Cubies level is a simple way of describing a cube position
-      (i.e. an element of the Rubik group).  This level is intended to
-      communicate with the "outside world" and for basic manipulation.
-      It does not need to be as efficient as subsequent coordinate
-      systems.  *)
-  module Cubies =
-  struct
+  type edge = UR | UF | UL | UB | DR | DF | DL | DB | FR | FL | BL | BR
+  let int_of_edge = function
+    | UR -> 0 | UF -> 1 | UL -> 2 | UB -> 3 | DR -> 4 | DF -> 5
+    | DL -> 6 | DB -> 7 | FR -> 8 | FL -> 9 | BL -> 10 | BR -> 11
+  let edge_int = [| UR; UF; UL; UB; DR; DF; DL; DB; FR; FL; BL; BR |]
+  let edge_name = [| "UR"; "UF"; "UL"; "UB"; "DR"; "DF";
+                     "DL"; "DB"; "FR"; "FL"; "BL"; "BR" |]
 
+  type t = {
+    corner_perm: int array;
+    corner_rot: int array;              (* 0,1,2: CW rotation of 120° *)
+    edge_perm: int array;
+    edge_flip: int array;               (* 0: not flipped, 1: flipped *)
+  }
 
-  end
+  let unsafe_make corners edges =
+    let corner_perm = Array.make 8 (-1)
+    and corner_rot = Array.make 8 0 in
+    List.iteri (fun i (c,o) ->
+                  corner_perm.(i) <- int_of_corner c;
+                  corner_rot.(i) <- o
+               ) corners;
+    let edge_perm = Array.make 12 (-1)
+    and edge_flip = Array.make 12 0 in
+    List.iteri (fun i (e,o) ->
+                  edge_perm.(i) <- int_of_edge e;
+                  edge_flip.(i) <- o;
+               ) edges;
+    { corner_perm = corner_perm; corner_rot = corner_rot;
+      edge_perm = edge_perm; edge_flip = edge_flip }
 
+  let make ~corner ~edge =
+    if List.length corner <> 8 then invalid_arg "Rubik.Cubie.make: 8 corners!";
+    if List.length edge <> 12 then invalid_arg "Rubik.Cubie.make: 12 edges!";
+    (* Check injectivity & orientations valuesa *)
+    let corner_seen = Array.make 8 false in
+    List.iter (fun (c,o) ->
+                 let ci = int_of_corner c in
+                 if corner_seen.(ci) then
+                   invalid_arg("Rubik.Cubie.make: " ^ corner_name.(ci)
+                               ^ " present twice");
+                 corner_seen.(ci) <- true;
+                 if o < 0 || o > 2 then
+                   invalid_arg("Rubik.Cubie.make: " ^ corner_name.(ci)
+                               ^ " orientation must be 0, 1, or 2");
+              ) corner;
+    let edge_seen = Array.make 12 false in
+    List.iter (fun (e,_) ->
+                 let ei = int_of_edge e in
+                 if edge_seen.(ei) then
+                   invalid_arg("Rubik.Cubie.make: " ^ edge_name.(ei)
+                               ^ " present twice");
+              ) edge;
+    (* Make sure the orientation is globally coherent *)
+    unsafe_make corner (List.map (fun (e,o) -> (e, if o then 1 else 0)) edge)
+
+  let corner cube c =
+    let ci = int_of_corner c in
+    (corner_int.(cube.corner_perm.(ci)), cube.corner_rot.(ci))
+  let edge cube e =
+    let ei = int_of_edge e in
+    (edge_int.(cube.edge_perm.(ei)), cube.edge_flip.(ei) <> 0)
+
+  (* The elements corresponding to the generators (a clockwise move of
+     the corresponding face). *)
+  let move_F =
+    unsafe_make [UFL,1; DLF,2; ULB,0; UBR,0; URF,2; DFR,1; DBL,0; DRB,0]
+      [UR,0;  FL,1;  UL,0;UB,0;DR,0;  FR,1;  DL,0;DB,0;  UF,1; DF,1;  BL,0;BR,0]
+  let move_B =
+    unsafe_make [URF,0; UFL,0; UBR,1; DRB,2; DFR,0; DLF,0; ULB,2; DBL,1]
+      [UR,0;UF,0;UL,0;  BR,1;  DR,0;DF,0;DL,0;  BL,1;  FR,0;FL,0;  UB,1; DB,1]
+  let move_L =
+    unsafe_make [URF,0; ULB,1; DBL,2; UBR,0; DFR,0; UFL,2; DLF,1; DRB,0]
+      [UR,0;UF,0;  BL,0;  UB,0;DR,0;DF,0;  FL,0;  DB,0;FR,0;  UL,0; DL,0;  BR,0]
+  let move_R =
+    unsafe_make [DFR,2; UFL,0; ULB,0; URF,1; DRB,1; DLF,0; DBL,0; UBR,2]
+      [FR,0;  UF,0;UL,0;UB,0;  BR,0;  DF,0;DL,0;DB,0;  DR,0;  FL,0;BL,0;  UR,0]
+  let move_U =
+    unsafe_make [UBR,0; URF,0; UFL,0; ULB,0; DFR,0; DLF,0; DBL,0; DRB,0]
+      [UB,0; UR,0; UF,0; UL,0;  DR,0;DF,0;DL,0;DB,0;FR,0;FL,0;BL,0;BR,0]
+  let move_D =
+    unsafe_make [URF,0; UFL,0; ULB,0; UBR,0; DLF,0; DBL,0; DRB,0; DFR,0]
+      [UR,0;UF,0;UL,0;UB,0;  DF,0; DL,0; DB,0; DR,0; FR,0;FL,0;BL,0;BR,0]
+  let move = function
+    | F -> move_F | B -> move_B | L -> move_L
+    | R -> move_R | U -> move_U | D -> move_D
+
+  (* Group operations *)
+  let id = {
+    corner_perm = Array.init 8 (fun i -> i); (* 8 corners *)
+    corner_rot = Array.make 8 0;
+    edge_perm = Array.init 12 (fun x -> x); (* 12 edges *)
+    edge_flip = Array.make 12 0;
+  }
+
+  let is_identity cube = cube = id
+
+  let mul a b = {
+    corner_perm = Array.init 8 (fun x -> a.corner_perm.(b.corner_perm.(x)));
+    corner_rot =
+      (let o x = (a.corner_rot.(b.corner_perm.(x)) + b.corner_rot.(x)) mod 3 in
+       Array.init 8 o);
+    edge_perm = Array.init 12 (fun x -> a.edge_perm.(b.edge_perm.(x)));
+    edge_flip =
+      (let o x = (a.edge_flip.(a.edge_perm.(x)) + b.edge_flip.(x)) land 0x1 in
+       Array.init 12 o);
+  }
+
+  (* inverse in Z/3Z *)
+  let inv3 x = if x = 0 then 0 else 3 - x
+
+  let inv cube =
+    let corner_perm = Array.make 8 (-1) in
+    for i = 0 to 7 do corner_perm.(cube.corner_perm.(i)) <- i done;
+    let edge_perm = Array.make 12 (-1) in
+    for i = 0 to 11 do edge_perm.(cube.edge_perm.(i)) <- i done;
+    {
+      corner_perm = corner_perm;
+      corner_rot = Array.init 8 (fun i -> inv3 cube.corner_rot.(i));
+      edge_perm = edge_perm;
+      edge_flip = cube.edge_flip;         (* in Z/2Z, -x = x *)
+    }
 
 end
+
--- rubik/rubik.mli
+++ rubik/rubik.mli
@@ -17,29 +17,106 @@
 
 (** Rubik cube solvers. *)
 
-type generators = F | B | L | R | U | D
-    (** Standard Rubik group genrrators.  The letters respectively
-        stand for the clockwise rotations of the faces `Front',
-        `Back', `Left', `Right', `Up' and `Down'. *)
+(** Standard Rubik group generators.  The letters respectively stand
+    for the clockwise rotations of the faces `Front', `Back', `Left',
+    `Right', `Up' and `Down'. *)
+type generator = F | B | L | R | U | D
 
 
-(** Various coordinate systems with corresponding multipliction
+(** {2 Coordinate systems}
+
+    Various coordinate systems with corresponding multipliction
     tables.  *)
-module Coord :
+
+(** The {{:http://kociemba.org/math/cubielevel.htm}cubies
+    coordinates}.  *)
+module Cubie :
 sig
-  (** The {{:http://kociemba.org/math/cubielevel.htm}cubies
-      coordinates}.  *)
-  module Cubies :
-  sig
-    type t
-      (** A permutation of the Rubik cube (i.e. an element of the Rubik
-          group).  This is immutable. *)
+  type corner =
+    | URF                               (** Up Right Front *)
+    | UFL                               (** Up Front Left *)
+    | ULB                               (** Up Left Back *)
+    | UBR                               (** Up Back Right *)
+    | DFR                               (** Down Front Right *)
+    | DLF                               (** Down Left Front *)
+    | DBL                               (** Down Back Left *)
+    | DRB                               (** Down Right Back *)
+  (** Naming of the 8 corner cubies.
+      Here is a "graphical" representation of the naming scheme:
+      {v
+            ULB ---- UBR
+             /|       /|
+            / |      / |
+           / DBL    /  |
+         ULF ---- URF DRB
+          |  /     |  /
+          | /      | /
+          |/       |/
+         DLR ---- DFR			v}
+      *)
 
-    val mul : t -> generators -> t
-      (** Right multiply by a generator. *)
+  (** The 12 edge cubies. *)
+  type edge =
+    | UR                                (** Up Right *)
+    | UF                                (** Up Front *)
+    | UL                                (** Up Left *)
+    | UB                                (** Up Back *)
+    | DR                                (** Down Right *)
+    | DF                                (** Down Front *)
+    | DL                                (** Down Left *)
+    | DB                                (** Down Back *)
+    | FR                                (** Front Right *)
+    | FL                                (** Front Left *)
+    | BL                                (** Back Left *)
+    | BR                                (** Back Right *)
 
-    val is_identity : t -> bool
-      (** The premutation is the Rubik cube home state i.e., the
-          identity (neutral element of the group). *)
-  end
+  type t
+    (** Construct a scrambling of the Rubik cube (i.e. a permutation
+        of the home state, i.e. an element of the Rubik group).  It is
+        immutable. *)
+
+  val make : corner: (corner * int) list -> edge: (edge * bool) list -> t
+    (** Construct a permutation of the Rubik cube.
+
+        The [corner] argument is the list of corners and their
+        orientation (0, 1, or 2) by which URF, UFL, ULB, UBR, DFR,
+        DLF, DBL, DRB are replaced (in that order).  For example, for
+        the [F] move, the URF cubie is replaced by the ULF and rotated
+        120 degrees clockwise, so corner is [[(ULF,1); (DLF,2);
+        (ULB,0); (UBR,0); (URF,2); (DFR,1); (DBL,0); (DRB,0)]].
+
+        The [edge] argument is the same as the [corner] but for edge
+        cubies (where the orientation is [true] or [false] depending
+        of whether the edge is flipped or not).  The order in which
+        the replacements are given is UR, UF, UL, UB, DR, DF, DL, DB,
+        FR, FL, BL, BR.
+
+        @raise Invalid_argument if the data does not describe a valid
+        permuation of the cube.  *)
+
+  val corner : t -> corner -> corner * int
+    (** [corner cube c] returns the corner and its orientation by which
+        [c] is replaced in the [cube]. *)
+  val edge : t -> edge -> edge * bool
+    (** [edge cube e] returns the edge and its flip state by which [e]
+        is replaced in the [cube]. *)
+
+  val move : generator -> t
+    (** [move g] the  generator [g] expressed at the cubie level. *)
+
+  val id : t
+    (** The home state of the Rubik cube (i.e. the identity of the
+        Rubik group).  *)
+
+  val mul : t -> t -> t
+    (** [mul g1 g2] is the group multiplication of [g1] and [g2].  It
+        corresponds to permute the home cube with [g1] and then apply
+        to the result the permutation represented by [g2]. *)
+
+  val inv : t -> t
+    (** [inv g] inverse permutation. *)
+
+  val is_identity : t -> bool
+    (** The premutation is the Rubik cube home state i.e., the
+        identity (neutral element of the group). *)
 end
