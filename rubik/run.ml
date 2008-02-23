f867f49213f64114c64177d5cb4b6089ed59e1cc
chris_77 <>
2008-02-23 18:21:16 +0000

    - Template for the executable program (for tests for now).

---
+++ rubik/run.ml
@@ -0,0 +1,24 @@
+(* File: run.ml
+
+   Copyright (C) 2008
+
+     Christophe Troestler <chris_77@users.sourceforge.net>
+     WWW: http://math.umh.ac.be/an/software/
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2.1 or
+   later as published by the Free Software Foundation, with the special
+   exception on linking described in the file LICENSE.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+   LICENSE for more details. *)
+
+
+open Rubik
+
+let () =
+  let mul1, prun1 = Phase1.initialize()
+  and mul2, prun2 = Phase2.initialize() in
+  ()
