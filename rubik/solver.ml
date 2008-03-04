09392603432cc46ca9fcedd93415de6f66e6ae9b
julie_87 <>
2008-03-04 23:56:34 +0000

    - Added tests for the 2 phases of the algorithm.

--- rubik/solver.ml
+++ rubik/solver.ml
@@ -23,9 +23,10 @@
 module Make(P:
   sig
     type t
+    module Move : Rubik.MoveT
     val compare : t -> t -> int
     val max_moves : int
-    val initialize : ?file:string -> unit -> (t -> Rubik.Move.t -> t) * (t -> int)
+    val initialize : ?file:string -> unit -> (t -> Move.t -> t) * (t -> int)
   end) =
 struct
 
@@ -44,7 +45,7 @@ struct
     (* [get_children p] returns the "children" of the permutation [p] (ie all
        the permutations we can reach by applying a single move to [p]) with
        the associated move. *)
-    let get_children p = List.map (fun move -> (mul p move, move)) Move.all in
+    let get_children p = List.map (fun move -> (mul p move, move)) P.Move.all in
     (* [aStar] recursively searches a sequence of moves to get into the goal
        state. (A* algorithm) *)
     let rec aStar opened =
@@ -52,7 +53,7 @@ struct
       if PQ.is_empty opened (* No more candidates. *) then no_seq()
       else
         let (p,seq,pcost) = PQ.take opened in
-        if is_in_goal p (* Get into the goal state! *) then seq
+        if is_in_goal p (* Get into the goal state! *) then List.rev seq
         else begin
           let children = get_children p in
           (* We update the set [opened] with the children of [p]. *)
@@ -78,8 +79,71 @@ struct
 end
 
 module Solver1 = Make(Rubik.Phase1)
+module Solver2 = Make(Rubik.Phase2)
 open Rubik
-let seq = Solver1.search_seq_to_goal
-  (Solver1.mul (Phase1.of_cube Cubie.id) (Move.make(F,3))) Phase1.in_G1
+
+let rec print1 s = (match s with
+  | [] -> ()
+  | m :: tl -> (let (g,i) = Phase1.Move.generator m in
+    let print_gen g = Printf.printf "Move: %s , %i \n%!" g i in
+    (match g with
+    | F -> print_gen "F"
+    | B -> print_gen "B"
+    | L -> print_gen "L"
+    | R -> print_gen "R"
+    | U -> print_gen "U"
+    | D -> print_gen "D");
+    print1 tl))
+
+let rec print2 s = (match s with
+  | [] -> ()
+  | m :: tl -> (let (g,i) = Phase2.Move.generator m in
+    let print_gen g = Printf.printf "Move: %s , %i \n%!" g i in
+    (match g with
+    | F -> print_gen "F"
+    | B -> print_gen "B"
+    | L -> print_gen "L"
+    | R -> print_gen "R"
+    | U -> print_gen "U"
+    | D -> print_gen "D");
+    print2 tl))
+
+let corner = [(Cubie.UFL,1); (Cubie.DLF,2);(Cubie.ULB,0); (Cubie.UBR,0);
+              (Cubie.URF,2); (Cubie.DFR,1); (Cubie.DBL,0); (Cubie.DRB,0)]
+
+let edge = [(Cubie.UF,false); (Cubie.UL,false); (Cubie.UB,false);
+            (Cubie.DR,false); (Cubie.DF,false); (Cubie.DL,false);
+            (Cubie.DB,false); (Cubie.FR,false); (Cubie.FL,false);
+            (Cubie.BL,false); (Cubie.BR,false); (Cubie.UR,false)]
+
+let () =
+  (*let cube = Cubie.make corner edge in*)
+  (*let cube = Solver1.mul (Phase1.of_cube Cubie.id) (Move.make(F,3)) in*)
+  (*let cube = Solver1.mul
+    (Solver1.mul (Phase1.of_cube Cubie.id) (Move.make(F,3))) (Move.make(R,2)) in*)
+  let moves = [F,3; R,2; U,1; B,3; D,1; L,2; R,3; U,2] in
+  let moves = List.map (fun m -> Cubie.move (Move.make m)) moves in
+  let cube = List.fold_left Cubie.mul Cubie.id moves in
+  let cubeP1 = Phase1.of_cube cube in
+  let seq1 = Solver1.search_seq_to_goal cubeP1 Phase1.in_G1 in
+  print1 seq1;
+  let rec apply cube seq =
+    match seq with
+    | [] -> cube
+    | m :: tl -> apply (Cubie.mul cube (Cubie.move m)) tl
+  in
+  let cube2 = apply cube seq1 in
+  let cubeP2 = Phase2.of_cube cube2 in
+  let seq2 = Solver2.search_seq_to_goal cubeP2 Phase2.is_identity in
+  print2 seq2;
+  let rec apply2 cube seq =
+    match seq with
+    | [] -> cube
+    | m :: tl -> apply2 (Cubie.mul cube (Phase2.Move.move m)) tl
+  in
+  let goal = apply2 cube2 seq2 in
+  if Cubie.is_identity goal then Printf.printf "Wouhouuu!! \n%!"
+
+
 
 
