c0410e36ac2f72dece50322fc44f2fb087ad49a8
chris_77 <>
2008-03-09 18:02:30 +0000

    - Added more difficult cube.

--- rubik/run.ml
+++ rubik/run.ml
@@ -77,9 +77,15 @@ let mul_and_print move c m =
 
 (********* Test *********)
 let () =
-  let moves = [F,3; R,2; U,1; B,3; D,1; L,2; R,3; U,2; R,3; B,1; F,3;
-               F,1; R,1; U,3; B,1; D,2; L,3; B,2
-              ] in
+  let moves = [F,3; R,2; U,1; B,3; D,1; L,2; R,3; U,2; R,3; B,1;
+               F,3; F,1; R,1; U,3; B,1; D,2; L,3; B,2 ] in
+  (* Patterns from http://www.math.ucf.edu/~reid/Rubik/patterns.html *)
+  (* let moves = [F,2; B,2; R,2; L,2; U,2; D,2] in *)
+  (* let moves = [U,3; L,3; U,3; F,3; R,2; B,3; R,1; F,1; U,1; B,2;
+               U,1; B,3; L,1; U,3; F,1; U,1; R,1; F,3] in *)
+  (*let moves = [R,2; L,3; D,1; F,2; R,3; D,3; R,3; L,1; U,3; D,1; R,1;
+               D,1; B,2; R,3; U,1; D,2] in*)
+
   let moves = List.map (fun m -> Cubie.move (Move.make m)) moves in
   let cube = List.fold_left Cubie.mul Cubie.id moves in
   display_cube cube;
@@ -128,5 +134,5 @@ let () =
       List.iter M.make (List.map Phase2.Move.generator seq2)*)
 
 (*   flush stdout; *)
-  ignore(wait_next_event [Button_down])
+  (* ignore(wait_next_event [Button_down]) *)
 
