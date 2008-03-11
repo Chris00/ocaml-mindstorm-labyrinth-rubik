1b7e5dc35a1cf402f44917b1a70e8268ccc4d6ad
chris_77 <>
2008-03-11 13:07:43 +0000

    - License notification & authors.

--- rubik/run.ml
+++ rubik/run.ml
@@ -1,3 +1,21 @@
+(* File: run.ml
+
+   Copyright (C) 2008
+
+     Julie de Pril <julie_87@users.sourceforge.net>
+     Dany Maslowski <dan_86@users.sourceforge.net>
+     Marc Ducobu <el_marcu@users.sourceforge.net>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2.1 or
+   later as published by the Free Software Foundation, with the special
+   exception on linking described in the file LICENSE.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+   LICENSE for more details. *)
+
 open Printf
 open Rubik
 module Motor = Mindstorm.Motor
