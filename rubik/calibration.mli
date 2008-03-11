524b568f75baee8c6699dd82717bbb783e8b4699
el_marcu <>
2008-03-11 10:58:40 +0000

    - improved the documentation of 'calibrate_webcam'

--- rubik/calibration.mli
+++ rubik/calibration.mli
@@ -3,7 +3,7 @@
 val calibrate_webcam : ?snapshot_file:string -> unit -> unit
 (** [calibrate_webcam ?snapshot_file _] hepls for putting the picking color
     area on good position on the snapshot. [snapshot_file] is the way to
-    access to she ppm snapshot_file.*)
+    access to she ppm snapshot file.*)
 
 (** To calibrate the mechanics *)
 val calibrate_mechanics : unit -> unit
