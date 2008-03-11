e37c98d3ef58cc814a73809791dd41b9f462a1af
chris_77 <>
2008-03-11 09:50:45 +0000

    - Created run.ml to write the interactions of the various modules and   solver.ml to experiment with various search strategies.

--- rubik/solver.ml
+++ rubik/solver.ml
@@ -1,4 +1,4 @@
-(* File: run.ml
+(* File: solver.ml
 
    Copyright (C) 2008
 
@@ -29,68 +29,9 @@ module Solver2 = Search.Make(Phase2)
 let move1 = Cubie.move
 let move2 = Phase2.Move.move
 
-(********* Modules for the physical part *********)
-module Motor = Mindstorm.Motor
-
-(* let conn = let bt = *)
-(*   if Array.length Sys.argv < 2 then ( *)
-(*     printf "%s <bluetooth addr>\n" Sys.argv.(0); *)
-(*     exit 1; *)
-(*   ) *)
-(*   else Sys.argv.(1) in Mindstorm.connect_bluetooth bt *)
-
-(* module C = *)
-(* struct *)
-(*   let conn = conn *)
-(*   let motor_fighter = Motor.a *)
-(*   let motor_hand = Motor.b *)
-(*   let motor_pf = Motor.c *)
-(*   let push_hand_port = `S2 *)
-(*   let push_fighter_port = `S1 *)
-(*   let cog_is_set_left = true *)
-(* end *)
-
-(*module M = Translator.Make(C)*)
-
-(********* Graphical part *********)
-open Graphics
-
-let x0 = 10
-and y0 = 20
-and len_sq = 30
-let cube_colors = (blue, magenta, yellow, red, white, green)
-
-let () =
-  let img_width = x0 + 12 * len_sq + 180 in
-  let img_high = y0 + 9 * len_sq in
-  open_graph (sprintf " %ix%i-100+50" img_width img_high);
-  set_window_title "Rubik cube";
-  set_color (rgb 219 219 219);
-  fill_rect 0 0 img_width img_high
-
-
-let display_cube cube = Display.cube x0 y0 cube_colors len_sq cube
-
-let mul_and_print move c m =
-  let cube = Cubie.mul c (move  m) in
-(*   print_cube cube; *)
-(*   Unix.sleep 2; *)
-  cube
 
 (********* Test *********)
-let () =
-  let moves = [F,3; R,2; U,1; B,3; D,1; L,2; R,3; U,2; R,3; B,1;
-               F,3; F,1; R,1; U,3; B,1; D,2; L,3; B,2 ] in
-  (* Patterns from http://www.math.ucf.edu/~reid/Rubik/patterns.html *)
-  (* let moves = [F,2; B,2; R,2; L,2; U,2; D,2] in *)
-  (* let moves = [U,3; L,3; U,3; F,3; R,2; B,3; R,1; F,1; U,1; B,2;
-               U,1; B,3; L,1; U,3; F,1; U,1; R,1; F,3] in *)
-  (*let moves = [R,2; L,3; D,1; F,2; R,3; D,3; R,3; L,1; U,3; D,1; R,1;
-               D,1; B,2; R,3; U,1; D,2] in*)
-
-  let moves = List.map (fun m -> Cubie.move (Move.make m)) moves in
-  let cube = List.fold_left Cubie.mul Cubie.id moves in
-  display_cube cube;
+let find_first cube =
 
   (********* Phase 1 *********)
   let cubeP1 = Phase1.of_cube cube in
@@ -125,6 +66,11 @@ let () =
     );
   end solutions;
 
+  solutions
+;;
+
+
+let _ =
   (* Restart *)
   let nmoves = 12 in
   let seq1' = Solver1.solutions cubeP1 nmoves in
@@ -144,12 +90,3 @@ let () =
       ignore(wait_next_event [Button_down])
     );
   end solutions;
-
-
-  (********* Physical part *********)
-  (*  List.iter M.make (List.map Phase1.Move.generator seq1);
-      List.iter M.make (List.map Phase2.Move.generator seq2)*)
-
-(*   flush stdout; *)
-  (* ignore(wait_next_event [Button_down]) *)
-
