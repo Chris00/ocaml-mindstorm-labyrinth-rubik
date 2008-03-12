ece8ae2b414adb20c143099c6c91c08df7d8e389
chris_77 <>
2008-03-12 10:15:45 +0000

    - Reverted to vlc (after trying "webcam").

--- rubik/snapshot.ml
+++ rubik/snapshot.ml
@@ -17,55 +17,20 @@ open Graphics
    LICENSE for more details. *)
 
 
-(* At the beginning "gqcam --dump" was expected to deliver webcam
-   snapshots.  However when using this command line snapshot facility,
-   the colors did not come out well.  So vlc was chosen because the
-   color adjustment is nice and it is cross platform. *)
+(* FIXME: this file needs some cleanup *)
 
-(* According to http://forum.videolan.org/viewtopic.php?f=7&t=16492
-   the snapshot command should be available in remote-control mode
-   but "key" is not recognized in the current vlc version. *)
-(* vlc -h *)
-(* vlc --help --advanced *)
-let vlc_remote =
-  "vlc --intf=rc v4l:// \
-	--snapshot-path /tmp/ --snapshot-prefix ocaml --snapshot-format png"
-
-(* This approach saves snapshots in a file that we can read in
-   (execute "vlc -p image" for more info).  The "-I rc" starts the
-   remote-control interface, so no display is shown and we have an
-   easy way to quit.  *)
-IFDEF WIN32 THEN
-let vlc_remote = "c:/\"Program files\"/VideoLAN/VLC/vlc -I rc \
-        dshow:// :dshow-adev=\"none\" --vout=image --image-out-replace \
-        --image-out-format=png --image-out-prefix=rubik"
-let vlc = "c:/Program files/VideoLAN/VLC/vlc"
-let vlc_args fname =
-  [| "--intf=rc"; "dshow://"; "--vout=image"; "--image-out-replace";
-     "--image-out-format=png"; "--image-out-prefix=" ^ fname |]
-ELSE
-IFDEF MACOS THEN
-  (* FIXME: url ? *)
-(* let vlc_remote = "vlc -I rc ???:// -V image --image-out-replace \ *)
-(* 	--image-out-format png --image-out-prefix " *)
-let vlc_remote = "vlc --intf=rc v4l:// --vout=image --image-out-replace \
-	--image-out-format=png --image-out-prefix="
-let vlc = "/usr/bin/vlc"
-let vlc_args fname =
-  [| "--intf=rc"; "v4l://"; "--vout=image"; "--image-out-replace";
-     "--image-out-format=png"; "--image-out-prefix=" ^ fname |]
-ELSE
-  (* Unix *)
-let vlc_remote = "vlc --intf=rc v4l:// --vout=image --image-out-replace \
-	--image-out-format=png --image-out-prefix="
-let vlc = "/usr/bin/vlc"
-let vlc_args fname =
-  [| "--intf=rc"; "v4l://"; "--vout=image"; "--image-out-replace";
-     "--image-out-format=png"; "--image-out-prefix=" ^ fname |]
-ENDIF
-ENDIF
+type color = int
 
 let imagemagick_convert = "convert"
+
+
+let convert fname1 fname2 =
+  let cmd = imagemagick_convert ^ " " ^ fname1 ^ " " ^ fname2 in
+  Printf.eprintf "### %s\n%!" cmd;
+  ignore(Sys.command cmd)
+
+(* Helpers
+ ***********************************************************************)
 
 let copy fname1 fname2 =
   let buf = String.create 8192 in
@@ -78,49 +43,6 @@ let copy fname1 fname2 =
   done;
   close_in fin;
   close_out fout
-
-let convert fname1 fname2 =
-  let cmd = imagemagick_convert ^ " " ^ fname1 ^ " " ^ fname2 in
-  Printf.eprintf "### %s\n%!" cmd;
-  ignore(Sys.command cmd)
-
-
-type color = int
-
-type webcam = {
-  pid : int;
-  png : string
-}
-
-IFDEF WIN32 THEN
-let start () =
-  let fname = Filename.temp_file "rubik" "" in
-  (* FIXME: If one connects a pipe to input commands to vlc, vlc does
-     not work as expected.  With [Unix.open_process_in],
-     [Unix.close_process_in] does not terminate the process.  So we
-     use the pid and [kill]. *)
-  let pid =
-    Unix.create_process vlc (vlc_args fname)
-      Unix.stdin Unix.stdout Unix.stderr in
-  { pid = pid;  png = fname ^ ".png" }
-    ELSE
-let start () =
-  let fname = Filename.temp_file "rubik" "" in
-  (* FIXME: If one connects a pipe to input commands to vlc, vlc does
-     not work as expected.  With [Unix.open_process_in],
-     [Unix.close_process_in] does not terminate the process.  So we
-     use the pid and [kill]. *)
-  match Unix.fork() with
-  | 0 -> Unix.execv vlc (vlc_args fname)
-  | pid -> { pid = pid;  png = fname ^ ".png" }
-ENDIF
-;;
-
-let stop w =
-  Unix.kill w.pid Sys.sigkill;
-  Unix.unlink w.png
-;;
-
 
 let must_wait_longer = function
   | None -> true
@@ -137,8 +59,122 @@ let rec wait_for_file fname =
     Unix.sleep 1;
     wait_for_file fname
   end
+;;
 
-let convert_rb img =
+let exchange_rb img =
+  let ret = Array.make_matrix (Array.length img) (Array.length img.(0)) red in
+  let rgb_components c =
+    (c lsr 16) land 0xFF,
+    (c lsr 8) land 0xFF,
+    c land 0xFF in
+  for i = 0 to (Array.length img) - 1
+  do
+    for j = 0 to (Array.length img.(0)) -1
+    do
+      let (r,g,b) = rgb_components img.(i).(j) in
+      ret.(i).(j) <- Graphics.rgb b g r
+    done;
+  done;
+  ret
+
+
+(************************************************************************)
+
+IFDEF WIN32 THEN
+(* This approach saves snapshots in a file that we can read in
+   (execute "vlc -p image" for more info).  The "-I rc" starts the
+   remote-control interface, so no display is shown and we have an
+   easy way to quit.  *)
+let vlc_remote = "c:/\"Program files\"/VideoLAN/VLC/vlc -I rc \
+        dshow:// :dshow-adev=\"none\" --vout=image --image-out-replace \
+        --image-out-format=png --image-out-prefix=rubik"
+let vlc = "c:/Program files/VideoLAN/VLC/vlc"
+let vlc_args fname =
+  [| "--intf=rc"; "dshow://"; "--vout=image"; "--dshow-adev=\"none\"";
+     "--image-out-replace"; "--image-out-format=png";
+     "--image-out-prefix=" ^ fname |]
+ELSE
+IFDEF MACOS THEN
+
+(* MacOS
+   ----------------------------------------------------------------------
+
+   vlc does not connect to a webcam *)
+let vlc = "/usr/bin/vlc"
+let vlc_args fname =
+  [| "--intf=rc"; "v4l://"; "--vout=image"; "--image-out-replace";
+     "--image-out-format=png"; "--image-out-prefix=" ^ fname |]
+
+ELSE
+
+(* Unix
+   ----------------------------------------------------------------------
+
+   At the beginning "gqcam --dump" was expected to deliver webcam
+   snapshots.  However when using this command line snapshot
+   facility, the colors did not come out well.
+
+   We then tried vlc using
+
+   * the rc or telnet interface but there is currently no way of
+   requesting a snapshot through these interfaces -- it should be
+   available in future version according to
+   http://forum.videolan.org/viewtopic.php?f=7&t=16492. (vlc
+   --intf=rc v4l:// --snapshot-path /tmp/ --snapshot-prefix ocaml
+   --snapshot-format png)
+
+   * the continuous file saving mode (along the lines of "vlc
+   --intf=rc v4l:// --vout=image --image-out-replace
+   --image-out-format=png --image-out-prefix=rubik") but no color
+   correction is the applied
+
+   Finally, we tried the simple 'webcam' program (from xawtv, Debian
+   package: webcam) to grab the frames but it simply does not work...
+
+   => We use vlc for its potential future...
+*)
+
+let vlc = "vlc"
+let vlc_args fname =
+  [| "--intf=rc"; "v4l://"; "--vout=image"; "--image-out-replace";
+     "--image-out-format=png"; "--image-out-prefix=" ^ fname |]
+
+ENDIF
+ENDIF
+
+type webcam = {
+  pid : int;
+  png : string;
+}
+
+let start () =
+  let fname = Filename.temp_file "rubik" "" in
+  (* FIXME: If one connects a pipe to input commands to vlc, vlc does
+     not work as expected.  With [Unix.open_process_in],
+     [Unix.close_process_in] does not terminate the process.  So we
+     use the pid and [kill]. *)
+  IFDEF WIN32 THEN (
+    let pid =
+      Unix.create_process vlc (vlc_args fname)
+        Unix.stdin Unix.stdout Unix.stderr in
+    { pid = pid;  png = fname ^ ".png" }
+  )
+  ELSE (
+    match Unix.fork() with
+    | 0 -> Unix.execv vlc (vlc_args fname)
+    | pid -> { pid = pid;  png = fname ^ ".png" }
+  )
+  ENDIF
+;;
+
+let stop w =
+  Unix.kill w.pid Sys.sigkill;
+  Unix.unlink w.png
+;;
+
+
+
+let exchange_rb img =
   let ret = Array.make_matrix (Array.length img) (Array.length img.(0)) red in
   let rgb_components c =
     (c lsr 16) land 0xFF,
@@ -160,7 +196,7 @@ let take w =
   copy w.png png;
   let ppm = Filename.temp_file "rubik_" ".ppm" in
   convert png ppm;
-  let img = convert_rb (Ppm.as_matrix_exn ppm) in
+  let img = exchange_rb (Ppm.as_matrix_exn ppm) in
   Unix.unlink png;
   Unix.unlink ppm;
   img
