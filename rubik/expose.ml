2cc1cb05c4a243d9cdbd1204405fe5dcbaaf673a
julie_87 <>
2008-03-13 00:16:05 +0000

    - Added slides that presents the inverse movement.

--- rubik/expose.ml
+++ rubik/expose.ml
@@ -21,12 +21,16 @@ let () =
   save "F1" (Cubie.id >> (F,1));
   save "F2" (Cubie.id >> (F,2));
   save "F3" (Cubie.id >> (F,3));
-
+  save "R1" (Cubie.id >> (R,1));
+  save "R1R3" (Cubie.id >> (R,1) >> (R,3));
   save "F1R2" (Cubie.id >> (F,1) >> (R,2));
-
   save "F1B2" (Cubie.id >> (F,1) >> (B,2));
-
   save "F1R2U3" (Cubie.id >> (F,1) >> (R,2) >> (U,3));
+
+  let scrambled = Cubie.id >> (D,1) >> (L,2) >> (U,3) >> (F,1) in
+  save "scrambled" scrambled;
+  save "scrambledR1" (scrambled >> (R,1));
+  save "scrambledR3" (scrambled >> (R,3));
 
 
 (* Local Variables: *)
