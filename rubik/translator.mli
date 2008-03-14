db3761ef77d498edd47d91f05d2fe4fb9f635dea
chris_77 <>
2008-03-14 18:58:54 +0000

    - Proper attribution.

--- rubik/translator.mli
+++ rubik/translator.mli
@@ -1,4 +1,18 @@
-(* File: translator.mli *)
+(* File: translator.mli
+
+   Copyright (C) 2008
+
+     Dany Maslowski <dan_86@users.sourceforge.net>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2.1 or
+   later as published by the Free Software Foundation, with the special
+   exception on linking described in the file LICENSE.
+
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+   LICENSE for more details. *)
 
 module type TranslatorT =
 sig
